eea35bf6bc005752c6fa841410fbecc049f54966
metrolog <plafett@gmail.com>
2025-06-21 08:34:59 -0400

    Уже рабочий вариант, но можно лучше

--- dz_script1.sh
+++ dz_script1.sh
@@ -4,19 +4,24 @@
 SCRIPT_NAME=$(basename "$0")
 
 # Файл для хранения PID
-PID_FILE="./pid.${SCRIPT_NAME}"
+PID_FILE="./pid_${SCRIPT_NAME}"
 
-if [ ! -f "$PID_FILE" ]; then
-    echo "процесс с таким PID звпущен"    
-# записали номер pid в файл
-echo "файла с PID нет. Значит создадим и запустим скрипт" 
-echo $$ > "$PID_FILE"
-while true; do
-sleep 2
-done
-#if [[ echo $PID_FILE == $$ ]]; then 
-else echo "файл с PID уже есть"
-#else echo "файл с PID уже есть. Значит проверим, есть ли в списке процесс с таким PID"
-#echo "процесс с таким PID звпущен"
-fi
-#fi
+if [ ! -s "$PID_FILE" ]; then
+    echo "файлик с PID пустой"
+    echo $$ > "$PID_FILE"
+    while true; do
+    sleep 2
+    done
+    else 
+        var=`cat $PID_FILE`
+        if [ `ps aux | grep $var | awk '{print $2}' | head -n 1` -eq `cat $PID_FILE` ]; then
+        echo "Такой PID уже запущен. магии не будет"
+        exit 1
+        else 
+        echo "pid $$ отличается от того, что записан в файл. Ура магия!"
+        echo $$ > "$PID_FILE"
+        while true; do
+        sleep 2
+        done
+        fi
+fi 
