8b37f21258164a38ca9ea7491aa1336b03ad90e1
metrolog <plafett@gmail.com>
2025-06-27 07:30:35 -0400

    create logger

---
+++ 3/loger.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+if [[ "$#" -lt 3 ]]; then
+    echo "less than 3 arguments specified"
+    exit 1
+fi
+
+#function character_conversions { 
+OLD_LOG_NAME=`grep "_FILE" text.txt | awk -F '"' 'NR==1{print $2}'`
+OLD_LOG_NAME_ASYNC=`grep "Async" text.txt | awk -F '"' '{print $2}'`
+OLD_LOG_CATEGORY=`grep "level=" text.txt | awk -F '"' '{print $2}'`
+OLD_LOG_LEVEL=`grep "level=" text.txt | awk -F '"' '{print $4}'`
+
+NEW_LOG_NAME=`echo $1 | tr '[:lower:]' '[:upper:]'`
+NEW_LOG_CATEGORY=`echo $2 | tr '[:lower:]' '[:upper:]'`
+NEW_LOG_LEVEL=`echo $3 | tr '[:lower:]' '[:upper:]'`
+
+sed -i "s/$OLD_LOG_NAME/$NEW_LOG_NAME\_FILE/g" text.txt
+sed -i "s/$OLD_LOG_NAME_ASYNC/$NEW_LOG_NAME\_ROLL/g" text.txt
+sed -i "s/$OLD_LOG_LEVEL/$NEW_LOG_LEVEL/g" text.txt
+sed -i "s/$OLD_LOG_CATEGORY/$NEW_LOG_CATEGORY/g" text.txt
+cat text.txt 
